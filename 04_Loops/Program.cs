2318abf7ccf129fd36ed29f13caa668645e8daa4
sahindesign85 <sahintutus@yandex.com>
2024-10-11 16:06:55 +0300

    4. and 5. lessons were uploaded.

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Döngüsü
+            //Döngüler
+
+            //For(x;y;z)
+            //x: başlangıç
+            // y; bitiş
+            //artış azalış
+            // yöntem ismini yazdıktan sonra iki defa tab tuşuna basarsan bilgisayar formatı ayarlar.
+
+            //int i;
+            //for (i = 1; i <=5; i++) 
+            // // i=1 ifadesi i değişkenini 1 arttırmak için
+            // // i<5 ifadesi süslü parantez içindeki işlemi 5 defa yap demek.
+            // // i++ ifadesi artarak gideceğini ifade etmek için.
+            //{
+            //    Console.WriteLine("C# Eğitim Kampı");
+
+            //}
+
+
+
+
+
+            //for (int i = 1; i <= 20; i++) //int değeri ekrana yazdırılarak kullanırsa satır satır 1 arttırır.
+            //{
+            //Console.WriteLine(i);
+            //}
+
+            //for (int i = 3; i <= 50; i+=3) // 3 sayısını 50'ye kadar 3'er 3'er arttırır.
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+
+
+
+            //Console.WriteLine("Ekrana istediğiniz satır kadar 'Yaşasın Cumhuriyet' yazılacaktır.");
+            //Console.Write("Lütfen ekrana yazılmasını istediğiniz satır sayısını giriniz: ");
+            //int finishvalue = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= finishvalue; i++)
+            //{
+            //    Console.WriteLine("Yaşasın Cumhuruiyet");
+            //}
+
+            #endregion
+
+            #region For Döngüsü ile Karar Yapıları
+
+            //// 1'den 100'e kadar olan sayılardan 5'e bölünebilenleri sıralama
+            //for (int i = 1; i <=100; i++)
+            //{
+            //    if ( i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+            //// Sayıları 1'den 10'a kadar sayıların tolam değerğnğ bulma
+            //int totalvalue = 0;
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    totalvalue += i;
+            //}
+            //Console.WriteLine(totalvalue);
+
+
+
+            //// 1'den 20'ye kadar olan sayıları bulup sıralama ve daha sonra toplamını bulma
+            //int totalvalue = 0;
+            //for (int i = 1; i < 20; i++)
+            //{ 
+            //if(i%2 == 0)
+            //    {
+            //        totalvalue += i;
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.WriteLine("-------");
+            //Console.WriteLine(totalvalue);
+
+
+
+            // 1 ie 50 arasında 7'ye bölünebilen kaç tane tam sayı olduğunu bulma ve o sayıları toplama
+            //int count = 0;
+            //for (int i = 1; i <=50; i++)
+            //{
+            //    if (i % 7 == 0) //i'nin değeri 7'ye bölümünden kalan sıfır  ise
+            //    {
+            //        count += 1;
+            //    }
+            //}
+
+            //Console.WriteLine (count);
+
+
+
+            // Bakterilerin her saat iki katına çıkma problemi
+            //int bacterium = 1;
+            //for (int i = 1; i <= 24; i++)
+            //{
+            //    bacterium *= 2;
+            //    Console.WriteLine(i + ".Saat Sonunda: " + bacterium + " Bakteri üremiştir.");
+
+            //}
+
+            #endregion
+
+            #region While Döngüsü (Şart sağlandığı Sürece işlem yapar) - (Bunu hiç kullanmadım, örnekleri Organizer'e at!)
+
+            // Bir şart sağlanana kadar süslü parantez içindeki işlemleri gerçekleştirir.
+            // if ile for arası bir işlem.
+            //while(şart)
+            //{
+            //    işlemler
+            //}
+
+            //int i=1;
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine("Merhaba Döngüler"); // i değişkeni 10  sayısına ulaşana kadar süslü parantez içindeki işlemleri gerçekleştirir. 
+            //    i++;
+            //}
+
+
+
+            // 1 ie 10 arasında 3'e tam bölünebilen sayıları bulma
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+
+            //// 1'den 10'a kadar olan sayıları bulma ve onları toplama
+            //int i = 1;
+            //int sum = 0;
+            //while (i<=10)
+            //{
+            //    sum += i;
+            //    i++;
+            //    Console.WriteLine(i); // Murat hocanın uygulamasında bu satır yok. Sonuçları görmek için ben ekledim.
+            //}
+            //Console.WriteLine(sum);
+
+            #endregion
+
+            #region Örnek Sınav Sorusu
+
+            ////Kalvyeden girilen 3 basamaklı sayının basamaklarının toplamını hesaplayan kodo yazınız.
+            ////456
+            //Console.WriteLine("*****Girilen Sayının Basamakları Toplamını Bulma*****");
+            //Console.Write("Bir Sayı Giriniz: ");
+            //int number = int.Parse(Console.ReadLine());
+            //int ones, tens, hundreds, sum;
+
+            //ones = number % 10;
+            //tens = (number % 100) / 10;
+            //hundreds = number / 100;
+     
+            //Console.WriteLine(ones + "-" + tens + "-" + hundreds);
+
+            //sum = ones + tens + hundreds;
+
+            //Console.WriteLine(sum);
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
---
+++ 05_LoopWithStars/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Alt alta 10 tane yıldız oluşturma
+
+            //for (int i = 1; i <= 10; i++)
+
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+            #region Yan yana 10 tane yıldız oluşturma
+
+            //for (int i = 1; i <= 10; i++)
+
+            //{
+
+            //    Console.Write("*");
+            //}
+
+            #endregion
+
+            #region Alt alta 10 tane yıldız oluşturma, her  satırda 10tane yıldız olsun
+
+            //for (int i = 1; i <= 10; i++)
+
+            //{
+
+            //    Console.WriteLine("**********");
+            //}
+
+            #endregion
+
+
+            #region Dik Üçgen Oluşturma (for içinde for döngüsü oluşturma. (ayrıca çalış buna Table'da id döndürürken lazım olacak)
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters Dik Üçgen Oluşturma 
+            ////azalması için i =5 ve küçük eşit yerine büyükeşit yapıldı.
+
+            //for (int i = 5; i >= 1; i--) //gittikçe azalması için ++ yerine -- yapıldı
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Dik ve Ters Dik Üçgen Beraber Uygulama (Üçgen artan şekilde taban sayısına ulaşınca bu sefer gittikçe azalacak)
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int k = 4; k >= 1; k--) //Zaten üçgen tabanı 5 olduğu için bir eksiği olan 4 tanımlandı i için
+            //{
+            //    for (int m = 1; m <= k; m++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Baklava Dilimi Oluşturma
+
+
+            ////Baklava diliminin üstteki tarafı başlangıç
+            //int n = 5;
+
+            //for (int i = 1; i <= n; i++)  // Dik üçgeni 5 satır olana kadar arttırır.
+
+            //{
+            //    for (int j = n - 1; j > 0; j--) // satırın başlangıcına 4 boşluk bırakana kadar çevirir. 
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+
+            //    for (int k = 1; k <= 2 * i - 1; k++) // sırayla 1-3-5-7 yıldız koymak için formül (2*3=6 6-1=5 şeklinde)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            ////Baklava dilimiminin üstteki tarafı bitiş
+
+            ////BaklavaDilimiminin alttaki tarafı başlangıç
+
+            //for(int i=n-1; i>=1; i--)
+            //{
+            //    for (int j= n -1; j> 0; j--)
+            //    {
+            //        Console.Write(" "); 
+            //    }
+
+            //    for( int k = 1;k <= 2 * i - 1;k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+
+            //}
+
+            ////Baklava dilimiminin alttaki tafı bitiş
+
+            #endregion
+
+            #region Piramit oluşturma
+
+            //int n = 5;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    //boşluklar için (boşlulşar yukardan aşağı azalarak gidiyor.)
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    //Yıldızlar için (yıldızşar yukardan aşağı artarak gidiyor)
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Ters Piramit oluşturma
+
+            //int n = 50;
+            //for (int i = n; i >= 1; i--)
+            //{
+            //    //boşluklar için 
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    //Yıldızlar için 
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
